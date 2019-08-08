890c552bf39997f3b7412e96a4a0497c6a5268d6
AzvenusK <azvenuskestel19@outlook.com>
2019-08-08 21:01:02 +0530

    Commit1
    MinimalistBasketball/Assets *

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+   
+    [SerializeField]
+    Game gamePrefab;
+
+    public Game CurrentGame;
+    public Resetter Reset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartGame(0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (CurrentGame != null)
+        {
+            CheckForCurrentGameOver();
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Reset.CheckForReset();
+    }
+
+    void CheckForCurrentGameOver()
+    {
+        if (CurrentGame.currLevelObj == null && CurrentGame.gameOver)
+        {
+            Destroy(CurrentGame.gameObject);
+            Debug.Log("Game Over!");
+        }
+    }
+    public void StartGame(int level)
+    {
+        CurrentGame = Instantiate(gamePrefab) as Game;
+        CurrentGame.gameObject.name = "Game";
+
+        CurrentGame.InitLevel(level, 3);
+    }
+}
